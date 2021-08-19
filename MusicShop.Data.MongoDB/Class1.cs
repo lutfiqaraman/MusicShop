9d5378182ff672be7f6c861ade9aef0f3afb1ebb
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-19 22:17:05 +0300

    Rename some projects

---
+++ MusicShop.Data.MongoDB/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MusicShop.Data.MongoDB
+{
+    public class Class1
+    {
+    }
+}
---
+++ MusicShop.Data.SQL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MusicShop.Data.SQL
+{
+    public class Class1
+    {
+    }
+}

90238d875e054eaacd9f80aa6d8b6d3c548f48cb
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-20 11:53:38 +0300

    create configurations for Artist and Music tables

---
+++ MusicShop.Data.SQL/Configurations/ArtistConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicShop.Core.Models;
+using System;
+
+namespace MusicShop.Data.SQL.Configurations
+{
+    public class ArtistConfiguration : IEntityTypeConfiguration<Artist>
+    {
+        public void Configure(EntityTypeBuilder<Artist> builder)
+        {
+            builder
+                .HasKey(a => a.Id);
+
+            builder
+                .Property(a => a.Id)
+                .UseIdentityColumn();
+
+            builder
+                .Property(m => m.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .ToTable("Artists");
+        }
+    }
+}

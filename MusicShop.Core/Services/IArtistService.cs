bcf0ca73718833d4465807964d1534e4dfc819af
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-20 03:48:37 +0300

    Building the services of the project

---
+++ MusicShop.Core/Services/IArtistService.cs
@@ -0,0 +1,15 @@
+﻿using MusicShop.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MusicShop.Core.Services
+{
+    public interface IArtistService
+    {
+        Task<IEnumerable<Artist>> GetAllArtists();
+        Task<Artist> GetArtistById(int id);
+        Task<Music> CreateArtist(Artist artist);
+        Task UpdateArtist(Artist updateArtist, Artist artist);
+        Task DeleteArtist(Artist artist);
+    }
+}
---
+++ MusicShop.Core/Services/IMusicService.cs
@@ -0,0 +1,16 @@
+﻿using MusicShop.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MusicShop.Core.Services
+{
+    public interface IMusicService
+    {
+        Task<IEnumerable<Music>> GetAllWithArtists();
+        Task<Music> GetMusicById(int id);
+        Task<IEnumerable<Music>> GetMusicsByArtistId(int artistId);
+        Task<Music> CreateMusic(Music music);
+        Task UpdateMusic(Music updateMusic, Music music);
+        Task DeleteMusic(Music music);
+    }
+}

04da31a9246c3ebe4e38cf6f7d47f4bd0cdddb57
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-21 12:21:53 +0300

    create mongodb service

---
+++ MusicShop.Core/Models/Composer.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MusicShop.Core.Models
+{
+    public class Composer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ MusicShop.Core/Repositories/IComposerRepository.cs
@@ -0,0 +1,15 @@
+﻿using MusicShop.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MusicShop.Core.Repositories
+{
+    public interface IComposerRepository
+    {
+        Task<IEnumerable<Composer>> GetAllComposers();
+        Task<Composer> GetComposerById(int id);
+        Task<Composer> Create(Composer composer);
+        Task<bool> Delete(int id);
+        void Update(int id, Composer composer);
+    }
+}

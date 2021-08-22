7d6ff3ebc8489d7a99b40cb92f9b6c1d856abe36
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-22 21:22:20 +0300

    connect mongodb with the project

--- MusicShop.API/Startup.cs
+++ MusicShop.API/Startup.cs
@@ -4,7 +4,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
 using MusicShop.Core;
+using MusicShop.Core.Repositories;
+using MusicShop.Data.MongoDB.Repository;
+using MusicShop.Data.MongoDB.Setting;
 using MusicShop.Data.SQL;
 
 namespace MusicShop.API
@@ -21,13 +25,31 @@ namespace MusicShop.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
+            //START :: Configuration for SQL Server
             services.AddDbContext<MusicStoreDbContext>(
-                options => options.UseSqlServer(
+                options => 
+                options.UseSqlServer(
                     Configuration.GetConnectionString("Default"), x => x.MigrationsAssembly("MusicShop.Data.SQL")
                 ));
+            //END :: Configuration for SQL Server
+
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            // Configuration of MongoDB
+            services.Configure<Settings>(
+                options =>
+                {
+                    options.ConnectionString = Configuration.GetValue<string>("MongoDB:ConnectionString");
+                    options.Database = Configuration.GetValue<string>("MongoDB:Database");
+                });
+
+            services.AddSingleton<IMongoClient, MongoClient>(
+                options => new MongoClient(Configuration.GetValue<string>("MongoDB:ConnectionString")));
+
+            services.AddTransient<IDatabaseSettings, DatabaseSettings>();
+            services.AddScoped<IComposerRepository, ComposerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ MusicShop.Data.MongoDB/Repository/ComposerRepository.cs
@@ -0,0 +1,69 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MusicShop.Core.Models;
+using MusicShop.Core.Repositories;
+using MusicShop.Data.MongoDB.Setting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicShop.Data.MongoDB.Repository
+{
+    public class ComposerRepository : IComposerRepository
+    {
+        private readonly IDatabaseSettings Context;
+
+        public ComposerRepository(IDatabaseSettings context)
+        {
+            Context = context;
+        }
+
+        public async Task<Composer> Create(Composer composer)
+        {
+            await Context.Composers.InsertOneAsync(composer);
+            return composer;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            string convertedId = Convert.ToString(id);
+            ObjectId objectId = new ObjectId(convertedId);
+
+            FilterDefinition<Composer> filter = Builders<Composer>.Filter.Eq(m => m.Id, objectId);
+            DeleteResult deleteResult = await Context.Composers.DeleteOneAsync(filter);
+            bool result = deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+
+            return result;
+        }
+
+        public async Task<IEnumerable<Composer>> GetAllComposers()
+        {
+            return await Context.Composers.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<Composer> GetComposerById(int id)
+        {
+            string convertedId = Convert.ToString(id);
+            ObjectId objectId = new ObjectId(convertedId);
+
+            FilterDefinition<Composer> filter = Builders<Composer>.Filter.Eq(m => m.Id, objectId);
+            return await Context.Composers.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public void Update(int id, Composer composer)
+        {
+            string convertedId = Convert.ToString(id);
+            ObjectId objectId = new ObjectId(convertedId);
+
+            FilterDefinition<Composer> filter = Builders<Composer>.Filter.Eq(m => m.Id, objectId);
+            UpdateDefinition<Composer> update = Builders<Composer>
+                .Update
+                .Set("FirstName", composer.FirstName)
+                .Set("LastName", composer.LastName);
+
+            Context.Composers.FindOneAndUpdate(filter, update);
+        }
+    }
+}

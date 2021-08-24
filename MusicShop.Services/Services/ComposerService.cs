891e5917260e87f6aacab2501a8ed6dbe64d39de
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-24 21:59:52 +0300

    create ComposerService

---
+++ MusicShop.Services/Services/ComposerService.cs
@@ -0,0 +1,46 @@
+﻿using MusicShop.Core.Models;
+using MusicShop.Core.Repositories;
+using MusicShop.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicShop.Services.Services
+{
+    public class ComposerService : IComposerService
+    {
+        private readonly IComposerRepository Context;
+
+        public ComposerService(IComposerRepository context)
+        {
+            Context = context;
+        }
+
+        public Task<Composer> Create(Composer composer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Composer>> GetAllComposers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Composer> GetComposerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Composer composer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

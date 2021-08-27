fb16421c148460118b25a5315ec4c6681abd41ea
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-27 11:33:10 +0300

    Create GetAllMusics method

--- MusicShop.API/Controllers/MusicController.cs
+++ MusicShop.API/Controllers/MusicController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MusicShop.Core.Models;
+using MusicShop.Core.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MusicShop.API.Controllers
 {
@@ -6,6 +10,17 @@ namespace MusicShop.API.Controllers
     [ApiController]
     public class MusicController : ControllerBase
     {
+        private readonly IMusicService MusicService;
+        public MusicController(IMusicService musicService)
+        {
+            MusicService = musicService;
+        }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Music>>> GetAllMusics()
+        {
+            IEnumerable<Music> musics = await MusicService.GetAllWithArtists();
+            return Ok(musics);
+        }
     }
 }

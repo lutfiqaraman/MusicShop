d6af2499b1ef90f5f485f9fbbadd3525f84f773e
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-27 19:06:31 +0300

    Create the mapper

---
+++ MusicShop.API/Mappings/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MusicShop.API.ViewModels;
+using MusicShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicShop.API.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Domain to ViewModal
+            CreateMap<Music, MusicVM>();
+            CreateMap<Artist, ArtistVM>();
+
+            //ViewModal to Domain
+            CreateMap<MusicVM, Music>();
+            CreateMap<ArtistVM, Artist>();
+        }
+    }
+}

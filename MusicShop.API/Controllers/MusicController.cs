4b3b8936acd4637923d4d422dc5ab05b89853c9b
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-28 07:10:51 +0300

    Get musicById

--- MusicShop.API/Controllers/MusicController.cs
+++ MusicShop.API/Controllers/MusicController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using MusicShop.API.ViewModels;
 using MusicShop.Core.Models;
 using MusicShop.Core.Services;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace MusicShop.API.Controllers
@@ -25,10 +27,24 @@ namespace MusicShop.API.Controllers
         public async Task<ActionResult<IEnumerable<MusicVM>>> GetAllMusics()
         {
             IEnumerable<Music> musics = await MusicService.GetAllWithArtists();
-            IEnumerable<MusicVM> musicMapper = 
+            IEnumerable<MusicVM> lstMusic =
                 Mapper.Map<IEnumerable<Music>, IEnumerable<MusicVM>>(musics);
 
-            return Ok(musicMapper);
+            return Ok(lstMusic);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MusicVM>> GetMusicById(int id)
+        {
+            Music music = await MusicService.GetMusicById(id);
+
+            if (music == null)
+                return NotFound("Music cannot be found");
+            
+            MusicVM result = Mapper.Map<Music, MusicVM>(music);
+
+            return Ok(result);
+        }
+
     }
 }

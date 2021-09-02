30666c89b9470b9fcca7a49da54aa35ecaae0de1
lutfiqaraman <lutfiqaraman@gmail.com>
2021-09-02 21:45:27 +0300

    create artist controller

--- MusicShop.API/Controllers/ArtistController.cs
+++ MusicShop.API/Controllers/ArtistController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MusicShop.Core.Services;
 
 namespace MusicShop.API.Controllers
 {
@@ -6,5 +8,13 @@ namespace MusicShop.API.Controllers
     [ApiController]
     public class ArtistController : ControllerBase
     {
+        private readonly IArtistService ArtistService;
+        private readonly IMapper Mapper;
+
+        public ArtistController(IArtistService artistService, IMapper mapper)
+        {
+            ArtistService = artistService;
+            Mapper        = mapper;
+        }
     }
 }
--- MusicShop.API/Controllers/MusicController.cs
+++ MusicShop.API/Controllers/MusicController.cs
@@ -99,6 +99,5 @@ namespace MusicShop.API.Controllers
 
             return NoContent();
         }
-
     }
 }

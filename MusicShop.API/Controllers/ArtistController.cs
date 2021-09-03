38d975244e8f7a0f7d7b4d5b86b13d31384526c2
lutfiqaraman <lutfiqaraman@gmail.com>
2021-09-03 10:51:18 +0300

    create getallartists method

--- MusicShop.API/Controllers/ArtistController.cs
+++ MusicShop.API/Controllers/ArtistController.cs
@@ -1,6 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using MusicShop.API.ViewModels;
+using MusicShop.Core.Models;
 using MusicShop.Core.Services;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MusicShop.API.Controllers
 {
@@ -15,6 +20,16 @@ namespace MusicShop.API.Controllers
         {
             ArtistService = artistService;
             Mapper        = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ArtistVM>>> GetAllArtists()
+        {
+            IEnumerable<Artist> artists = await ArtistService.GetAllArtists();
+            IEnumerable<ArtistVM> lstArtist =
+                Mapper.Map<IEnumerable<Artist>, IEnumerable<ArtistVM>>(artists);
+
+            return Ok(lstArtist);
         }
     }
 }

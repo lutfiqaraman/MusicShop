2cdc8f814b1fc1b79edd343a216e578cdf14edd8
lutfiqaraman <lutfiqaraman@gmail.com>
2021-09-07 08:42:45 +0300

    Create user repository and user services

---
+++ MusicShop.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using MusicShop.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MusicShop.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync();
+        Task<User> Authenticate(string username, string password);
+        Task<User> Create(User user, string password);
+        void Update(User user, string password = null);
+        void Delete(int id);
+    }
+}
---
+++ MusicShop.Core/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using MusicShop.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MusicShop.Core.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+        Task<User> GetUserById();
+        Task<User> Authenticate(string username, string password);
+        Task<User> Create(User user, string password);
+        void Update(User user, string password = null);
+        void Delete(int id);
+    }
+}

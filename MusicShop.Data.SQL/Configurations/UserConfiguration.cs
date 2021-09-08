79be868b125da0e4cd7ead6988345d018951e118
lutfiqaraman <lutfiqaraman@gmail.com>
2021-09-08 09:30:05 +0300

    Create user configuration

---
+++ MusicShop.Data.SQL/Configurations/UserConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicShop.Data.SQL.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasKey(u => u.Id);
+
+            builder
+                .Property(m => m.FirstName)
+                .UseIdentityColumn();
+
+            builder
+                .Property(u => u.Username)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .Property(u => u.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .ToTable("Users");
+        }
+    }
+}

80631112215cbfc9b3efbfd9fbcc7fea37590d41
lutfiqaraman <lutfiqaraman@gmail.com>
2021-09-25 18:37:35 +0300

    Create login methods

---
+++ MusicShopMVC/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicShopMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> Logger;
+        private readonly IConfiguration Config;
+
+        private string URLBase
+        {
+            get
+            {
+                return Config.GetSection("BaseURL").GetSection("URL").Value;
+            }
+        }
+
+        public AccountController(ILogger<AccountController> logger, IConfiguration config)
+        {
+            Logger = logger;
+            Config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
